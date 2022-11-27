71ff0a0dccf5ecba6460da1a956922dd9c06fcf9
ChengWang <18771014894@163.com>
2022-11-27 22:21:51 +0000

    acc and dhw

--- behavior_analysis.py
+++ behavior_analysis.py
@@ -1,10 +1,10 @@
 from cutin_extraction import ScenarioExtraction
 import utils
-from parameter_distributions import ParameterDistribution
+from parameter_distributions import plot_kde
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KernelDensity
 import numpy as np
-import matplotlib
+
 
 plt.rcParams["font.family"] = "Times New Roman"
 
@@ -35,6 +35,7 @@ class BehaviorAnalysis:
         self.car_pairs_changing_offset = {}
         self.car_pairs_acc = {}
         self.dhw = {}
+        self.acc_dhw = []
 
     @staticmethod
     def trenddetector(list_of_index, array_of_data, order=1):
@@ -60,15 +61,16 @@ class BehaviorAnalysis:
             offset_t1_t3 = []
             time_t1_t3 = []
             accx_t3_t5 = []
+            inx_acc = []
 
             ego = car_pair[1]
             key_timestamps_t1 = int(car_pair[2][0])
             key_timestamps_t3 = int(car_pair[2][1])
             key_timestamps_t5 = int(car_pair[2][2])
-            # save dhw at T5
-            dhw_t5 = self.get_dhw(car_pair)
+            # save dhw at T5,inx is -1 at the last one
+            dhw_t5 = self.get_dhw(car_pair, -1)
             self.dhw[key] = dhw_t5
-            for ego_data in ego:
+            for inx, ego_data in enumerate(ego):
                 # should before t3, after that ego has no motivation to evade
                 if key_timestamps_t1 <= int(ego_data[self.frame]) <= key_timestamps_t3:
                     offset = float(ego_data[self.ego_offset])
@@ -78,10 +80,19 @@ class BehaviorAnalysis:
                 elif key_timestamps_t3 <= int(ego_data[self.frame]) <= key_timestamps_t5:
                     accx = float(ego_data[self.accx])
                     accx_t3_t5.append(accx)
+                    inx_acc.append(inx)
                 elif int(ego_data[self.frame]) > key_timestamps_t5:
                     break
-            # save acceleration
-            self.car_pairs_acc[key] = np.average(accx_t3_t5)
+            # save acceleration and corresponding dhw
+            acc_ave = np.average(accx_t3_t5)
+            self.car_pairs_acc[key] = acc_ave
+            min_acc = min(accx_t3_t5)
+            # define a threshold to ensure it decelerates actually
+            if min_acc < -0.1:
+                inx = inx_acc[accx_t3_t5.index(min_acc)]
+                dhw = self.get_dhw(car_pair, inx)
+                self.acc_dhw.append([min_acc, dhw])
+
             # in some cases, t1 to t3 are equal.
             if len(offset_t1_t3) < 2:
                 continue
@@ -135,23 +146,24 @@ class BehaviorAnalysis:
             ax_.grid(alpha=0.75)
 
         self.car_pairs_changing_offset = increasing_offset | decreasing_offset
-        self.plot_dx0()
-        self.plot_acc_dhw()
+        self.plot_offset()
+        self.plot_offset_3D()
+        self.plot_acc()
 
-    def get_dhw(self, car_pair):
+    def get_dhw(self, car_pair, inx_):
         ego = car_pair[1]
         cutin_car = car_pair[0]
-        inx_ = -1  # at T5, last one
 
-        height = float(cutin_car[inx_][self.width]) / 2 + float(ego[inx_][self.width]) / 2
-        rel_d_x = abs(float(ego[inx_][self.x]) - float(cutin_car[inx_][self.x])) - height
+        width = float(cutin_car[inx_][self.width]) / 2 + float(ego[inx_][self.width]) / 2
+        rel_d_x = abs(float(ego[inx_][self.x]) - float(cutin_car[inx_][self.x])) - width
 
         return rel_d_x
 
     def get_vy_dy(self, car_pairs, t1):
-        ''' calculate inverse TTC and corresponding ego velocity at timestamp t3'''
+        ''' calculate inverse TTC and corresponding ego velocity at timestamp t1'''
         rel_dy0 = []
         vy0 = []
+        vx0 = []
         ittc_y = []
         offset = []
         for key, car_pair in car_pairs.items():
@@ -161,16 +173,19 @@ class BehaviorAnalysis:
             key_timestamps_t3 = int(car_pair[2][1])
             key_timestamps_t5 = int(car_pair[2][2])
 
-            # for different timestamps T1 and T3
-            if t1 == 'T1':
-                timestamp = key_timestamps_t1
-            else:
-                timestamp = key_timestamps_t3
             # find the key timestamp
             for inx, car in enumerate(ego):
-                if int(car[self.frame]) == timestamp:
+                if int(car[self.frame]) == key_timestamps_t1:
                     inx_ = inx
                     break
+
+            # for different speeds
+            if t1 == 'lowSpeed':
+                if abs(float(ego[inx_][self.vx])) > 10:
+                    continue
+            elif t1 == 'highSpeed':
+                if abs(float(ego[inx_][self.vx])) <= 10:
+                    continue
 
             height = float(cutin_car[inx_][self.height]) / 2 + float(ego[inx_][self.height]) / 2
             rel_d_y = abs(float(ego[inx_][self.y]) - float(cutin_car[inx_][self.y])) - height
@@ -180,52 +195,97 @@ class BehaviorAnalysis:
 
             vy = abs(float(cutin_car[inx_][self.vy]))
             vy0.append(vy)
+            vx = abs(float(cutin_car[inx_][self.vx]))
+            vx0.append(vx)
 
             ittc_y.append(vy / rel_d_y)
 
-        return offset, ittc_y
+        return offset, ittc_y, vx0
 
-    def plot_dx0(self):
-        fig, ax = plt.subplots(1,2)
-        for inx, T in enumerate(['T1', 'T3']):
-            offset_changing, ittc_y_changing = self.get_vy_dy(self.car_pairs_changing_offset, T)
+    def plot_offset(self):
+        fig, ax = plt.subplots(1, 2)
+        for inx, T in enumerate(['lowSpeed', 'highSpeed']):
+            offset_changing, ittc_y_changing, vx_changing = self.get_vy_dy(self.car_pairs_changing_offset, T)
             coef = np.polyfit(ittc_y_changing, offset_changing, 1)
             poly1d_changing = np.poly1d(coef)
 
-            offset, ittc_y = self.get_vy_dy(self.car_pairs_constant_offset, T)
-            coef = np.polyfit(ittc_y, offset, 1)
-            poly1d_const = np.poly1d(coef)
-
+            offset, ittc_y, vx = self.get_vy_dy(self.car_pairs_constant_offset, T)
             ax[inx].scatter(ittc_y, offset, color='#FF8D57')
             ax[inx].scatter(ittc_y_changing, offset_changing, color='#C0ACA1')
-            ax[inx].plot(ittc_y, poly1d_const(ittc_y), color='#FF8D57')
-            ax[inx].plot(ittc_y_changing, poly1d_changing(ittc_y_changing), color='#C0ACA1')
+            ax[inx].set_xlabel(r'Lateral inverse TTC [1/s]')
             ax[inx].legend(['Constant offset', 'Shifting offset'])
+            if offset:
+                coef = np.polyfit(ittc_y, offset, 1)
+                poly1d_const = np.poly1d(coef)
+                ax[inx].plot(ittc_y, poly1d_const(ittc_y), color='#FF8D57')
+
+            ax[inx].plot(ittc_y_changing, poly1d_changing(ittc_y_changing), color='#C0ACA1')
             ax[inx].grid(alpha=0.7)
-        ax[0].set_xlabel(r'Lateral inverse TTC at $T_{1}$ [1/s]')
-        ax[1].set_xlabel(r'Lateral inverse TTC at $T_{3}$ [1/s]')
+        ax[0].set_title(r'$v_{\rm ego}$ < 10 m/s')
+        ax[1].set_title(r'10 m/s < $v_{\rm ego}$ < 22 m/s')
+
         ax[0].set_ylabel(r'Lateral ego offset at $T_{1}$ [m]')
-        ax[1].set_ylabel(r'Lateral ego offset at $T_{3}$ [m]')
 
+    def plot_offset_3D(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        T = None
+        offset_changing, ittc_y_changing, vx_changing = self.get_vy_dy(self.car_pairs_changing_offset, T)
+        ax.scatter3D(ittc_y_changing, vx_changing, offset_changing, color='#C0ACA1')
+        # Add x, y gridlines
+        ax.grid(visible=True, color='grey',
+                linestyle='-.', linewidth=0.3,
+                alpha=0.2)
+        ax.set_xlabel(r'$iTTC_{y}$ [1/s]')
+        ax.set_ylabel(r' $v_{\rm ego,T_{1}}$ [m/s]')
+        ax.set_zlabel(r'$\delta y$ [m]')
 
-    def plot_acc_dhw(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        offset, ittc_y, vx = self.get_vy_dy(self.car_pairs_constant_offset, T)
+        sctt = ax.scatter3D(ittc_y, vx, offset, color='#FF8D57')
+        ax.grid(visible=True, color='grey',
+                linestyle='-.', linewidth=0.3,
+                alpha=0.2)
+
+        ax.set_xlabel(r'$iTTC_{y}$ [1/s]')
+        ax.set_ylabel(r' $v_{\rm ego,T_{1}}$ [m/s]')
+        ax.set_zlabel(r'$\delta y$ [m]')
+
+        # fig.colorbar(sctt, ax=ax)
+
+    def plot_acc(self):
         accx = self.car_pairs_acc.values()
-        dhw = self.dhw.values()
+        accx_negative = []
+        dhw = []
+        for acc in self.acc_dhw:
+            accx_negative.append(acc[0])
+            dhw.append(acc[1])
 
         fig, (ax1, ax2) = plt.subplots(2)
         n, bins, patches = ax1.hist(accx, bins=6, density=True, histtype='bar', rwidth=1.0,
                                     facecolor='#D49AB3', edgecolor='black')
-        ax1.set_xlabel(r'Average accedelation during [$T_{3}$, $T_{5}$] [m/${\rm s}^{2}$]')
+
+        plot_kde(list(accx), bins, ax1, 0.1)
+        ax1.set_xlabel(r'Average acceleration during [$T_{3}$, $T_{5}$] [m/${\rm s}^{2}$]')
         ax1.set_ylabel('Density')
         ax1.grid(alpha=0.7)
 
-        n, bins, patches = ax2.hist(dhw, bins=6, density=True, histtype='bar', rwidth=1.0,
-                                    facecolor='#7A93B2', edgecolor='black')
-        ax2.set_xlabel(r'Dhw at $T_{3}$ [m]')
-        ax2.set_ylabel('Density')
+        ax2.scatter(dhw, accx_negative,color='#FF8D57')
+        ax2.set_xlabel(r'Dhw when reaching maximum deceleration [m]')
+        ax2.set_ylabel('Maximum deceleration [m/$s^{2}$]')
         ax2.grid(alpha=0.7)
         plt.subplots_adjust(hspace=0.4)
 
+    def plot_dhw(self):
+        dhw = self.dhw.values()
+        fig, ax = plt.subplots(1)
+        n, bins, patches = plt.hist(dhw, bins=6, density=True, histtype='bar', rwidth=1.0,
+                                    facecolor='#7A93B2', edgecolor='black')
+        plot_kde(list(dhw), bins, ax, 1)
+        ax.set_xlabel(r'Dhw at $T_{5}$ [m]')
+        ax.set_ylabel('Density')
+        ax.grid(alpha=0.7)
 
 def main():
     obj = utils.load_object("data.pickle")
